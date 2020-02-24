f325560f7b6c2f9cb5597da69d33c721b06da7ce
Grégoire <gregmialon@hotmail.com>
2020-02-24 13:00:03 +0100

    updated 10 and 8

--- figures.py
+++ figures.py
@@ -117,7 +117,8 @@ def plot_tree_interactive(X, y):
 
 
 def plot_kmeans_interactive(min_clusters=1, max_clusters=6):
-    from IPython.html.widgets import interact
+    #from IPython.html.widgets import interact
+    from ipywidgets import interact
     from sklearn.metrics.pairwise import euclidean_distances
     from sklearn.datasets.samples_generator import make_blobs
 
